2e95157ecab5840cdb71cca2371ec3765111dfcf
Chansu Park <hhosu107@gmail.com>
2023-05-30 23:42:38 +0900

    Rust Ch 13 example

--- chapters/13/closures/src/main.rs
+++ chapters/13/closures/src/main.rs
@@ -1,5 +1,18 @@
 use std::thread;
 
+struct T {
+    val: i32
+}
+
+impl T{
+    pub fn applier<F>(self, f: F) -> T
+    where
+        F: Fn(T) -> T
+    {
+        f(self)
+    }
+}
+
 fn main() {
     let list = vec![1, 2, 3];
     println!("Before defining closure: {:?}", list);
@@ -7,4 +20,9 @@ fn main() {
     thread::spawn(move || println!("From thread: {:?}", list))
         .join()
         .unwrap();
+
+    let a = 4;
+    let x = T{val: 5};
+    let add_a = |x: T| T{val: x.val + a};
+    println!("{}", x.applier(add_a).val)
 }
